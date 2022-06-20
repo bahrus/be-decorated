7e4b407ad81f40c98d74a8431af2481ad93712ab
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-06-20 07:04:17 -0400

    0.0.120

--- isoStorage.js
+++ isoStorage.js
@@ -1,1 +1,13 @@
-export const isoStorage = new Map();
+const isoStorage = new Map();
+export function store(ifWantsToBe, element, isoHelper) {
+    if (!isoStorage.has(ifWantsToBe)) {
+        isoStorage.set(ifWantsToBe, new WeakMap());
+    }
+    isoStorage.get(ifWantsToBe).set(element, isoHelper);
+}
+export function get(ifWantsToBe, element) {
+    if (isoStorage.has(ifWantsToBe)) {
+        return isoStorage.get(ifWantsToBe).get(element);
+    }
+    return undefined;
+}
--- isoStorage.ts
+++ isoStorage.ts
@@ -1,1 +1,15 @@
-export const isoStorage: Map<string, WeakMap<Element, any>> = new Map();
+const isoStorage: Map<string, WeakMap<Element, any>> = new Map();
+
+export function store(ifWantsToBe: string, element: Element, isoHelper: any){
+    if(!isoStorage.has(ifWantsToBe)){
+        isoStorage.set(ifWantsToBe, new WeakMap());
+    }
+    isoStorage.get(ifWantsToBe)!.set(element, isoHelper);
+}
+
+export function get(ifWantsToBe: string, element: Element){
+    if(isoStorage.has(ifWantsToBe)){
+        return isoStorage.get(ifWantsToBe)!.get(element);
+    }
+    return undefined;
+}
