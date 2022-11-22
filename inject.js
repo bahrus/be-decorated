e309672b41640bb9db8a4f16d1e5539bf5521622
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-11-22 16:10:20 -0500

    0.0.198

--- inject.js
+++ inject.js
@@ -1,28 +1,36 @@
-export function inject(into, me, of) {
-    const me0 = me[0];
-    const into0 = into[0];
-    if (into0 !== undefined)
-        Object.assign(into0, me0);
-    const me1 = me[1];
-    const into1 = into[1];
-    for (const key in me1) {
-        const into1val = into1[key];
-        if (into1val !== undefined) {
-            Object.assign(into1val, me1[key]);
-        }
-        else {
-            into1[key] = me1[key];
+export function inject({ mold, with: w, tbdSlots }) {
+    const into1 = mold[1];
+    if (w !== undefined) {
+        const me0 = w[0];
+        const into0 = mold[0];
+        if (into0 !== undefined)
+            Object.assign(into0, me0);
+        const me1 = w[1];
+        if (me1 !== undefined) {
+            for (const key in me1) {
+                const into1val = into1[key];
+                if (into1val !== undefined) {
+                    Object.assign(into1val, me1[key]);
+                }
+                else {
+                    into1[key] = me1[key];
+                }
+            }
         }
     }
-    if (of !== undefined) {
+    if (tbdSlots !== undefined) {
+        const props = Object.keys(tbdSlots);
         for (const val of Object.values(into1)) {
-            if (val?.of === 'tbd') {
-                val.of = of;
-            }
-            if (val?.abort?.of === 'tbd') {
-                val.abort.of = of;
+            for (const prop of props) {
+                if (val[prop] === 'tbd') {
+                    val[prop] = tbdSlots[prop];
+                }
+                const abort = val.abort;
+                if (abort?.[prop] === 'tbd') {
+                    abort[prop] = tbdSlots[prop];
+                }
             }
         }
     }
-    return into;
+    return mold;
 }
--- inject.ts
+++ inject.ts
@@ -1,27 +1,44 @@
 import {EventConfigs} from './types';
-export function inject<T extends [any, EventConfigs]>(into: T, me: T, of?: EventTarget): T{
-    const me0 = me[0];
-    const into0 = into[0];
-    if(into0 !== undefined) Object.assign(into0, me0);
-    const me1 = me[1];
-    const into1 = into[1];
-    for(const key in me1){
-        const into1val = into1[key];
-        if(into1val !== undefined){
-            Object.assign(into1val, me1[key]);
-        }else{
-            into1[key] = me1[key];
+interface injectArgs<T extends [any, EventConfigs]> {
+    mold: T,
+    with?: T,
+    tbdSlots?: {
+        of?: EventTarget,
+        on?: string,
+    } 
+}
+export function inject<T extends [any, EventConfigs]>({mold, with: w, tbdSlots}: injectArgs<T>): T{
+    const into1 = mold[1];
+    if(w !== undefined){
+        const me0 = w[0];
+        const into0 = mold[0];
+        if(into0 !== undefined) Object.assign(into0, me0);
+        const me1 = w[1];
+        if(me1 !== undefined){
+            for(const key in me1){
+                const into1val = into1[key];
+                if(into1val !== undefined){
+                    Object.assign(into1val, me1[key]);
+                }else{
+                    into1[key] = me1[key];
+                }
+            }
         }
+
     }
-    if(of !== undefined){
+    if(tbdSlots !== undefined){
+        const props = Object.keys(tbdSlots);
         for(const val of Object.values(into1)){
-            if(val?.of ===  'tbd'){
-                val.of = of;
-            }
-            if(val?.abort?.of === 'tbd'){
-                val.abort.of = of;
+            for(const prop of props){
+                if((val as any)[prop] === 'tbd'){
+                    (val as any)[prop] = (tbdSlots as any)[prop];
+                }
+                const abort = (val as any).abort;
+                if(abort?.[prop] === 'tbd'){
+                    abort[prop] = (tbdSlots as any)[prop];
+                }
             }
         }
     }
-    return into;
+    return mold;
 }
