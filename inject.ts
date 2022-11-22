5386995169ed5bb582b7c62b38b8fb32392fcd1d
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-11-22 07:07:14 -0500

    0.0.198

---
+++ inject.ts
@@ -0,0 +1,17 @@
+import {EventConfigs} from './types';
+export function inject<T extends [any, EventConfigs]>(into: T, me: T): T{
+    const me0 = me[0];
+    const into0 = into[0];
+    if(into0 !== undefined) Object.assign(into0, me0);
+    const me1 = me[1];
+    const into1 = into[1];
+    for(const key in me1){
+        const into1val = into1[key];
+        if(into1val !== undefined){
+            Object.assign(into1val, me1[key]);
+        }else{
+            into1[key] = me1[key];
+        }
+    }
+    return into;
+}
